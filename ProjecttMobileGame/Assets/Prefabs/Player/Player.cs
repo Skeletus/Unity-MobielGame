94cd860faee08aa462f95c65bc8bf0a6afb479aa
Skeletus <Samol_88@hotmail.com>
2024-01-25 23:08:34 -0500

    adding aiming control

--- ProjecttMobileGame/Assets/Prefabs/Player/Player.cs
+++ ProjecttMobileGame/Assets/Prefabs/Player/Player.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] JoyStick aimStick;
     [SerializeField] JoyStick moveStick;
     [SerializeField] CharacterController characterController;
     [SerializeField] float moveSpeed = 20f;
     [SerializeField] float turnSpeed = 30f;
+
     Vector2 moveInput;
+    Vector2 aimInput;
+
     Camera mainCam;
     CameraController cameraController;
 
@@ -16,8 +20,14 @@ public class Player : MonoBehaviour
     void Start()
     {
         moveStick.onStickValueUpdated += MoveInputUpdated;
+        aimStick.onStickValueUpdated += AimInputUpdated;
         mainCam = Camera.main;
         cameraController = FindObjectOfType<CameraController>();
+    }
+
+    void AimInputUpdated(Vector2 inputValue)
+    {
+        aimInput = inputValue;
     }
 
     void MoveInputUpdated(Vector2 inputValue)
@@ -25,17 +35,34 @@ public class Player : MonoBehaviour
         moveInput = inputValue;
     }
 
+    Vector3 StickInputToWorldDirection(Vector2 inputValue)
+    {
+        Vector3 rightDirection = mainCam.transform.right;
+        Vector3 upDirection = Vector3.Cross(rightDirection, Vector3.up);
+
+        return rightDirection * inputValue.x + upDirection * inputValue.y;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 rightDirection = mainCam .transform.right;
-        Vector3 upDirection = Vector3.Cross(rightDirection, Vector3.up);
-        Vector3 moveDirection = rightDirection * moveInput.x  + upDirection * moveInput.y;
+        Vector3 moveDirection = StickInputToWorldDirection(moveInput);
+        Vector3 aimDirection = moveDirection;
+
+        if(aimInput.magnitude!= 0)
+        {
+            aimDirection = StickInputToWorldDirection(aimInput);
+        }
+
+        if(aimDirection.magnitude!= 0)
+        {
+            float turnLerpAlpha = turnSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDirection, Vector3.up), 0.5f);
+        }
+
         characterController.Move(moveDirection * Time.deltaTime * moveSpeed);
         if (moveInput.magnitude != 0)
         {
-            float turnLerpAlpha = turnSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection, Vector3.up), 0.5f); 
             if (characterController != null)
             {
                 cameraController.AddYawInput(moveInput.x);
