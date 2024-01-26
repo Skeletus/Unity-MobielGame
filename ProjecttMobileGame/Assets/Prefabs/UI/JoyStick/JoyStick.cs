4193220c6536fccfc78c74809704f69eb00ac3ec
Skeletus <Samol_88@hotmail.com>
2024-01-25 20:27:34 -0500

    adding touch input to JoyStick

--- ProjecttMobileGame/Assets/Prefabs/UI/JoyStick/JoyStick.cs
+++ ProjecttMobileGame/Assets/Prefabs/UI/JoyStick/JoyStick.cs
@@ -5,19 +5,29 @@ using UnityEngine.EventSystems;
 
 public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] RectTransform thumbStickTransform;
+    [SerializeField] RectTransform backGroundTransform;
+    [SerializeField] RectTransform centerTransform;
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log($"On drag fired {eventData.position}");
+        Vector2 touchPosition = eventData.position;
+        Vector2 centerPosition = backGroundTransform.position;
+
+        Vector2 localOffSet = Vector2.ClampMagnitude(touchPosition - centerPosition, backGroundTransform.sizeDelta.x/2);
+        thumbStickTransform.position = centerPosition + localOffSet;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("PointerDown");
+        backGroundTransform.position = eventData.position;
+        thumbStickTransform.position = eventData.position;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("PointerUp");
+        backGroundTransform.position = centerTransform.position;
+        thumbStickTransform.position = backGroundTransform.position;
     }
 
     // Start is called before the first frame update
