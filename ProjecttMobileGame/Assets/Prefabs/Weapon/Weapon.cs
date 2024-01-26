3c4b3aae49d31d2241718da2b0bf6f9acc15ad74
Skeletus <Samol_88@hotmail.com>
2024-01-26 15:58:19 -0500

    adding rifle prefab and weapon abstract class

---
+++ ProjecttMobileGame/Assets/Prefabs/Weapon/Weapon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    [SerializeField] string attachedSlotTag;
+
+    public string GetAttachedSlotTag()
+    {
+        return attachedSlotTag;
+    }
+    public GameObject owner
+    {
+        get;
+        private set;
+    }
+
+    public void Init(GameObject _owner)
+    {
+        owner = _owner;
+    }
+
+    public void Equip()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void UnEquip()
+    {
+        gameObject.SetActive(false);
+    }
+}
