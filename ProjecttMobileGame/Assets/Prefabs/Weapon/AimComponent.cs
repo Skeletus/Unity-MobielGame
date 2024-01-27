f8a8f5a5ff674aa35495d5edd5b9405de9d762bb
Skeletus <Samol_88@hotmail.com>
2024-01-26 21:31:23 -0500

    adding aim component

---
+++ ProjecttMobileGame/Assets/Prefabs/Weapon/AimComponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimComponent : MonoBehaviour
+{
+    [SerializeField] Transform muzzle;
+    [SerializeField] float aimRange = 1000;
+    [SerializeField] LayerMask aimMask;
+
+    public GameObject GetAimTarget()
+    {
+        Vector3 aimStart = muzzle.position;
+
+        if(Physics.Raycast(aimStart, GetAimDirection(), out RaycastHit hitInfo, aimRange, aimMask))
+        {
+            return hitInfo.collider.gameObject;  
+        }
+
+        return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(muzzle.position, muzzle.position + GetAimDirection() * aimRange);
+    }
+
+    Vector3 GetAimDirection()
+    {
+        Vector3 muzzleDirection = muzzle.forward;
+        return new Vector3(muzzleDirection.x, 0f, muzzleDirection.z).normalized;
+    }
+}
