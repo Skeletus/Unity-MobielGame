8bb14816ecc6a32c23421b224f74a19be2e9efb8
Skeletus <Samol_88@hotmail.com>
2024-01-31 18:38:07 -0500

    adding node class

---
+++ ProjecttMobileGame/Assets/Prefabs/Framework/AI/BehaviorTree/BT_Node.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeResult
+{
+    Sucess,
+    Failure,
+    InProgress
+}
+
+public abstract class BT_Node
+{
+    public NodeResult UpdateNode()
+    {
+        if (!started)
+        {
+            started = true;
+            NodeResult executeResult = Execute();
+            if(executeResult != NodeResult.InProgress)
+            {
+                EndNode();
+                return executeResult;
+            }
+        }
+
+        NodeResult updateResult = Update();
+        if(updateResult != NodeResult.InProgress)
+        {
+            EndNode();
+        }
+        return updateResult;
+
+    }
+
+    protected virtual NodeResult Execute()
+    {
+        return NodeResult.Sucess;
+    }
+
+    protected virtual NodeResult Update()
+    {
+        return NodeResult.Sucess;
+    }
+
+    protected virtual void End()
+    {
+
+    }
+
+    private void EndNode()
+    {
+        started = false;
+        End();
+    }
+
+    bool started = false;
+}
