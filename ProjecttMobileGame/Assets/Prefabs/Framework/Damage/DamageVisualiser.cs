b2ea420be2846a38ffddc94b31b0adfd4ed82a00
Skeletus <Samol_88@hotmail.com>
2024-02-05 19:10:01 -0500

    adding damage visualizer component

---
+++ ProjecttMobileGame/Assets/Prefabs/Framework/Damage/DamageVisualiser.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageVisualiser : MonoBehaviour
+{
+    [SerializeField] Renderer mesh;
+    [SerializeField] Color DamageEmmisionColor;
+    [SerializeField] float BlinkSpeed = 2f;
+    [SerializeField] string EmmisionColorPropertyName = "_Addtion";
+    [SerializeField] HealthComponent healthComponent;
+    Color OrigionalEmissionColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material mat = mesh.material;
+        mesh.material = new Material(mat);
+
+        OrigionalEmissionColor = mesh.material.GetColor(EmmisionColorPropertyName);
+        healthComponent.onTakeDamage += TookDamage;
+    }
+
+    private void TookDamage(float health, float delta, float maxHealth, GameObject Instigator)
+    {
+        Color currentEmmisionColor = mesh.material.GetColor(EmmisionColorPropertyName);
+        if (Mathf.Abs((currentEmmisionColor - OrigionalEmissionColor).grayscale) < 0.1f)
+        {
+            mesh.material.SetColor(EmmisionColorPropertyName, DamageEmmisionColor);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color currentEmmisionColor = mesh.material.GetColor(EmmisionColorPropertyName);
+        Color newEmmisionColor = Color.Lerp(currentEmmisionColor, OrigionalEmissionColor, Time.deltaTime * BlinkSpeed);
+        mesh.material.SetColor(EmmisionColorPropertyName, newEmmisionColor);
+    }
+}
