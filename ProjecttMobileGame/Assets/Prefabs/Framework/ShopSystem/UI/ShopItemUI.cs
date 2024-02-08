6d2f6011bb8ad1d5da53589621a12b045344fb15
Skeletus <Samol_88@hotmail.com>
2024-02-08 00:01:34 -0500

    adding shop item UI

---
+++ ProjecttMobileGame/Assets/Prefabs/Framework/ShopSystem/UI/ShopItemUI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItemUI : MonoBehaviour
+{
+    [SerializeField] Image Icon;
+    [SerializeField] TextMeshProUGUI TitleText;
+    [SerializeField] TextMeshProUGUI PriceText;
+    [SerializeField] TextMeshProUGUI DescriptionText;
+
+    [SerializeField] Button button;
+    [SerializeField] Image GrayOutCover;
+
+    ShopItem item;
+
+    [SerializeField] Color InEfficientCreditColor;
+    [SerializeField] Color SurffiicentCreditColor;
+    // Start is called before the first frame update
+
+    public void Init(ShopItem item, int AvaliableCredits)
+    {
+        this.item = item;
+
+        Icon.sprite = item.ItemIcon;
+        TitleText.text = item.Title;
+        PriceText.text = "$" + item.Price.ToString();
+        DescriptionText.text = item.Description;
+
+        Refresh(AvaliableCredits);
+    }
+
+    private void Refresh(int avaliableCredits)
+    {
+        if (avaliableCredits < item.Price)
+        {
+            GrayOutCover.enabled = true;
+            PriceText.color = InEfficientCreditColor;
+        }
+        else
+        {
+            GrayOutCover.enabled = false;
+            PriceText.color = SurffiicentCreditColor;
+        }
+    }
+}
