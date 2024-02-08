179b7178dd362bc36c99daefe6d0215bf47961e7
Skeletus <Samol_88@hotmail.com>
2024-02-08 16:23:22 -0500

    adding all weapons and abilities to shop

--- ProjecttMobileGame/Assets/Prefabs/Weapon/InventoryComponent.cs
+++ ProjecttMobileGame/Assets/Prefabs/Weapon/InventoryComponent.cs
@@ -76,7 +76,18 @@ public class InventoryComponent : MonoBehaviour, IPurchaseListener
         Weapon itemAsWeapon = itemAsGameObject.GetComponent<Weapon>();
         if (itemAsWeapon == null) return false;
 
+        bool hasWeapon = true;
+        if (weapons.Count == 0)
+        {
+            hasWeapon = false;
+        }
+
         GiveNewWeapon(itemAsWeapon);
+
+        if (!hasWeapon)
+        {
+            EquipWeapon(0);
+        }
         return true;
     }
 
